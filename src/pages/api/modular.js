f311e35bf58a25d67a17caa010f3cad080ef9f29
Bestony <bestony@linux.com>
2023-04-15 19:31:17 +0800

    feat: add new action

---
+++ src/pages/api/modular.js
@@ -0,0 +1,16 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const KEY = process.env.OPENAIKEY;
+import { OpenAI } from "langchain/llms/openai";
+import { PromptTemplate } from "langchain/prompts";
+const template = "请帮我改写这段代码，让它更加模块化: {code} 如果已经很好了，则可以直接告诉我，这段代码很棒，请继续加油！";
+const codeReviewTemplate = new PromptTemplate({
+  template: template,
+  inputVariables: ["code"],
+});
+export default async function handler(req, res) {
+  const {code} = req.body
+  const model = new OpenAI({ openAIApiKey: KEY, temperature: 0.9 });
+  const prompts = await codeReviewTemplate.format({ code });
+  const response = await model.call(prompts);
+  res.status(200).json({ code: 0, data:response })
+}
